4ae5d58e6e057fa1f50329d2e89fd56a21d8fe1d
Delano Igbinoba <digbinoba@ufl.edu>
2018-10-12 22:00:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/delano/flashycards/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.delano.flashycards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the questions to the answer
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the questions to the answer
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+    //Onclick listener
+
+
+}
