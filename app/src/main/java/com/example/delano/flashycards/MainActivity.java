d1c74b4410f24c8c153d6785700f0a51cf357ca6
Delano Igbinoba <digbinoba@ufl.edu>
2018-11-03 00:52:14 -0400

    Able to change current flashcard. Added intent ability

--- app/src/main/java/com/example/delano/flashycards/MainActivity.java
+++ app/src/main/java/com/example/delano/flashycards/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.delano.flashycards;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     boolean isShowingAnswers = true;
@@ -34,67 +36,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Choice 1 is correct
-
-        findViewById(R.id.answer_1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set the questions to the answer
-                findViewById(R.id.answer_1).setBackgroundColor(getResources().getColor(R.color.turqBlue));
 
 
-            }
-        });
-        //Choice 2 and 3 are wrong
-        findViewById(R.id.answer_2).setOnClickListener(new View.OnClickListener() {
+        //Intent for adding new card
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Set the questions to the answer
-                findViewById(R.id.answer_1).setBackgroundColor(getResources().getColor(R.color.turqBlue));
-                findViewById(R.id.answer_2).setBackgroundColor(getResources().getColor(R.color.red));
-            }
-        });
-        findViewById(R.id.answer_3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set the questions to the answer
-                findViewById(R.id.answer_1).setBackgroundColor(getResources().getColor(R.color.turqBlue));
-                findViewById(R.id.answer_3).setBackgroundColor(getResources().getColor(R.color.red));
+                Intent intent = new Intent(MainActivity.this, add_card.class);
+                MainActivity.this.startActivityForResult(intent, 100);
             }
         });
 
-
-        //Reset the choice when pressing eye
-        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set the questions to the answer
-                ImageView imageView = (ImageView) findViewById(R.id.toggle_choices_visibility);
-                //Change answers to invisible when showing is false
-                if(isShowingAnswers == true) {
-                    imageView.setImageResource(R.drawable.ic_visibility_off_black_24dp);
-                    findViewById(R.id.answer_1).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.answer_2).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.answer_3).setVisibility(View.INVISIBLE);
-
-                    isShowingAnswers = false;
-                }else{
-                    isShowingAnswers = true;
-                    imageView.setImageResource(R.drawable.ic_visibility_black_24dp);
-                    findViewById(R.id.answer_1).setVisibility(View.VISIBLE);
-                    findViewById(R.id.answer_2).setVisibility(View.VISIBLE);
-                    findViewById(R.id.answer_3).setVisibility(View.VISIBLE);
-                    findViewById(R.id.answer_1).setBackgroundColor(getResources().getColor(R.color.peach));
-                    findViewById(R.id.answer_2).setBackgroundColor(getResources().getColor(R.color.peach));
-                    findViewById(R.id.answer_3).setBackgroundColor(getResources().getColor(R.color.peach));
-
-                }
-            }
-        });
 
 
     }
     //Onclick listener
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
 
+            ((TextView) findViewById(R.id.flashcard_question)).setText(string1);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(string2);
+        }
+    }
 
 }
